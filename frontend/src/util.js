cae4545eaad772efdb14fc61018607e3f750ddff
Daniel Lam <daniel.wingchi.lam@gmail.com>
2025-08-13 23:57:26 -0500

    chore: Update Base URL

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -1,5 +1,3 @@
-const API_BASE_URL = process.env.VITE_VERCEL_ENV === 'production'
-  ? process.env.VITE_API_URL
-  : 'http://localhost:8000/api';
+const API_BASE_URL = process.env.VITE_API_URL || 'http://localhost:8000/api';
 
 export { API_BASE_URL };
